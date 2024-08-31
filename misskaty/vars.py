a44d08886f6b2a4253508fb8acd1b2cfc701be0d
Budaxcomel <96898071+Budaxcomel@users.noreply.github.com>
2024-08-31 17:42:20 +0800

    Update vars.py

--- misskaty/vars.py
+++ misskaty/vars.py
@@ -48,19 +48,19 @@ else:
 # Optional ENV
 LOG_GROUP_ID = environ.get("LOG_GROUP_ID")
 USER_SESSION = environ.get("USER_SESSION")
-DATABASE_NAME = environ.get("DATABASE_NAME", "MissKatyDB")
-TZ = environ.get("TZ", "Asia/Jakarta")
+DATABASE_NAME = environ.get("DATABASE_NAME", "")
+TZ = environ.get("TZ", "Asia/Kuala_Lumpur")
 COMMAND_HANDLER = environ.get("COMMAND_HANDLER", "! /").split()
 SUDO = list(
     {
         int(x)
         for x in environ.get(
             "SUDO",
-            "617426792 2024984460",
+            "1358956715 5597816302",
         ).split()
     }
 )
-SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "YasirPediaChannel")
+SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "ImmanChannel")
 AUTO_RESTART = environ.get("AUTO_RESTART", False)
 OPENAI_KEY = environ.get("OPENAI_KEY")
 GOOGLEAI_KEY = environ.get("GOOGLEAI_KEY")
@@ -72,13 +72,13 @@ FORWARD_FROM_CHAT_ID = list(
         int(x)
         for x in environ.get(
             "FORWARD_FROM_CHAT_ID",
-            "-1001128045651 -1001455886928 -1001686184174",
+            "-1001603431697",
         ).split()
     }
 )
 # Forward To Chat ID
 FORWARD_TO_CHAT_ID = list(
-    {int(x) for x in environ.get("FORWARD_TO_CHAT_ID", "-1001210537567").split()}
+    {int(x) for x in environ.get("FORWARD_TO_CHAT_ID", "-1001603431697").split()}
 )
 FORWARD_FILTERS = list(set(environ.get("FORWARD_FILTERS", "video document").split()))
 BLOCK_FILES_WITHOUT_EXTENSIONS = bool(
